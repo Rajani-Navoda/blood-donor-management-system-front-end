f3749bd10eb86622a6e2b4290ad47abc03e3397b
Thevin Senath <85255608+ThevinSenath@users.noreply.github.com>
2023-08-01 06:53:18 +0530

    Revert "Rajani frontend"

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080',
-
-
-    firebaseConfig : {
-        apiKey: "AIzaSyD9stIWv7hCT6IVMHKZV_nMdEjpCTQL1IY",
-        authDomain: "blood-donor-management-s-a89e4.firebaseapp.com",
-        projectId: "blood-donor-management-s-a89e4",
-        storageBucket: "blood-donor-management-s-a89e4.appspot.com",
-        messagingSenderId: "513780462047",
-        appId: "1:513780462047:web:5004e45edd5b29a243946a",
-        measurementId: "G-6LQ76J9W67"
-      }
+    apiUrl: 'http://localhost:8080'
 };
